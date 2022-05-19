da10605a0c74027f554d5676c1c29bbf70f83608
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-05-19 18:45:47 -0400

    set password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
+function lowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function upperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getNumber() {
+  const numbers = "1234567890";
+}
+function getSymbol() {
+  const symbol = "!@#$%^&*()-_/?.,><";
+}
 
+var min = parseInt('8');
+var max = parseInt('128');
+
+var generatePassword = function() {
+  var passwordLength = window.prompt('Please type a number between 8 and 128 to declare how long you would like your password to be:');
+  
+  if (passwordLength < min || passwordLength > max) {
+    alert("Please try again. Enter a number between 8 and 128 to declare the length of the password.");
+    return generatePassword();
+  }
+  else {
+    alert('Your password will be ' + passwordLength + ' characters long.');
+  }
+  console.log(passwordLength);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
