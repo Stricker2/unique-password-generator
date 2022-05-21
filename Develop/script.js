a0f8fa59b213ba6f152edd46f724eafe973b232d
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-05-21 01:45:57 -0400

    add if statement to generate password when prompts are entered correctly

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,20 @@
 // password variables
-var passwordLength = '';
+var passwordLength = [];
 var choiceArray = [];
-var getLowers = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-var getUppers = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-var getNumbers = ('1','2','3','4','5','6','7','8','9','0');
-var getSymbols = ('!','@','#','$','%','^','&','*','(',')','_','-','<','>','/','?');
- 
-// define generatePassword function
-function generatePassword(){
-
-};
+var getLowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var getUppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var getNumbers = ['1','2','3','4','5','6','7','8','9','0'];
+var getSymbols = ['!','@','#','$','%','^','&','*','(',')','_','-','<','>','/','?'];
 
 // password prompts
 function passwordPrompts() {
+  choiceArray = [];
+
   passwordLength = parseInt(window.prompt("Please input a number between 8 and 128 to declare the character length of your password:"));
-  if (passwordLength < 8 || passwordLength > 128 || passwordLength('')) {
+  // BLANK RESPONSE OR NULL RESPONSE STILL GO THROUGH
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength == '' || passwordLength === null) {
     window.alert("The number you provided is invalid. Please provide a number between 8 and 128");
-    return false;
+    return passwordPrompts();
   }
 
   if (confirm("Do you want to include lowercase letters in your password? Click 'OK' if yes, or 'Cancel' if not.")) {
@@ -34,6 +32,12 @@ function passwordPrompts() {
   if (confirm("Do you want to include symbols in your password? Click 'OK' if yes, or 'Cancel' if not.")) {
     choiceArray = choiceArray.concat(getSymbols);
   }
+  return true;
+};
+
+// define generatePassword function
+function generatePassword(){
+
 };
 
 // Get references to the #generate element
@@ -42,10 +46,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() { 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // adding if statement so password only gets generated when prompt function returns true
+  var chosenPrompts = getPrompts();
 
-  passwordText.value = password;
+  if(chosenPrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
 };
 
 // Add event listener to generate button
